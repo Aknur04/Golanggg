42bd904be4e2e7a6c1787798cec5c8e8e7437ee7
Aknur Azamatkyzy <ak_azamatkyzy@kbtu.kz>
2023-12-03 02:19:58 +0600

    chapter 16

--- awesomeProject2/cmd/api/errors.go
+++ awesomeProject2/cmd/api/errors.go
@@ -6,8 +6,6 @@ import (
 )
 
 func (app *application) logError(r *http.Request, err error) {
-	// Use the PrintError() method to log the error message, and include the current
-	// request method and URL as properties in the log entry.
 	app.logger.PrintError(err, map[string]string{
 		"request_method": r.Method,
 		"request_url":    r.URL.String(),
@@ -51,4 +49,15 @@ func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Requ
 func (app *application) rateLimitExceededResponse(w http.ResponseWriter, r *http.Request) {
 	message := "rate limit exceeded"
 	app.errorResponse(w, r, http.StatusTooManyRequests, message)
+}
+
+func (app *application) invalidCredentialsResponse(w http.ResponseWriter, r *http.Request) {
+	message := "invalid authentication credentials"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
+
+func (app *application) invalidAuthenticationTokenResponse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("WWW-Authenticate", "Bearer")
+	message := "invalid or missing authentication token"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
 }
--- awesomeProject2/cmd/api/routes.go
+++ awesomeProject2/cmd/api/routes.go
@@ -11,12 +11,14 @@ func (app *application) routes() http.Handler {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 	router.HandlerFunc(http.MethodGet, "/v1/yoga", app.yogaHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/exercise", app.listexerciseHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.HealthCheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/exercise", app.createexerciseHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/exercise/:id", app.showexerciseHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/exercise/:id", app.updateexerciseHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/exercise/:id", app.deleteexerciseHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
-	return app.recoverPanic(app.rateLimit(router))
+	return app.recoverPanic(app.rateLimit(app.authenticate(router)))
 }
